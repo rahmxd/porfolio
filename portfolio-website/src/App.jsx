cd4ae3b1ae70e06bf5deeb34bd849e62fa423608
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-11-30 23:54:55 +0000

    set up components in app.jsx

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Portfolio from './components/portfolio/Portfolio'
+import Footer from './components/footer/Footer'
+import Testimonials from './components/testimonials/Testimonial'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Experience/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+    </>
   )
 }
 
--- portfolio-website/src/components/portfolio/Portfolio.jsx
+++ portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './portfolio.css'
+
 const Portfolio = () => {
   return (
     <div>Portfolio</div>
