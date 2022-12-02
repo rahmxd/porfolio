9af601306548b753dc7d5490b8b0d62d23e0ec05
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-12-02 11:34:59 +0000

    working navbar - links scroll to sections

--- portfolio-website/src/components/navbar/Navbar.jsx
+++ portfolio-website/src/components/navbar/Navbar.jsx
@@ -5,15 +5,16 @@ import { MdWorkOutline } from 'react-icons/md'
 import { AiOutlineUser } from 'react-icons/ai'
 import { BiBook } from 'react-icons/bi'
 import { BiMessageSquareDetail } from 'react-icons/bi'
-
+import { useState } from 'react'
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#home')
   return (
     <nav>
-      <a href="#" className='active'><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#portfolio"><MdWorkOutline/></a>
-      <a href="#contact"><BiMessageSquareDetail/></a>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdWorkOutline/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
