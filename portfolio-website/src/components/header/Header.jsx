dfa094c5e9726e00b01bf6a927deab4774c6214f
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-12-02 11:17:59 +0000

    added navbar and header styles

--- portfolio-website/src/components/header/Header.jsx
+++ portfolio-website/src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './header.css'
+import Cta from './Cta'
+// import profilePic from '../../assets/profilepic.jpeg'
+import me from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mohammed Ahmed</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <Cta/>
+        <HeaderSocials/>
+        <div className='me'>
+          <img src={me} alt="me" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+      </div>
+    </header>
   )
 }
 
--- portfolio-website/src/components/navbar/Navbar.jsx
+++ portfolio-website/src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { MdWorkOutline } from 'react-icons/md'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><MdWorkOutline/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
