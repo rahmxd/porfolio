bca294846ca2496df3e130127c3a9915fee66a47
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-12-03 22:10:23 +0000

    finished styling and media queries for about me component

--- portfolio-website/src/components/about/About.jsx
+++ portfolio-website/src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import './about.css'
+import aboutMe from '../../assets/aboutMe.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={aboutMe} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Full stack experience</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>1</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5 completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Autem rem mollitia, voluptas commodi doloribus incidunt 
+            veniam praesentium nihil dolorem omnis a placeat saepe velit 
+            dignissimos laborum perferendis porro. Quos, sapiente?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
